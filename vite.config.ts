6e91de66af0c9a324307388011324ad9f1963924
ActuallySaptarshi <saptarshispace18@gmail.com>
2026-09-01 19:26:26 +0530

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'vite';
 import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {reactRouter} from '@react-router/dev/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -11,8 +10,10 @@ export default defineConfig({
     hydrogen(),
     oxygen(),
     reactRouter(),
-    tsconfigPaths(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   build: {
     // Allow a strict Content-Security-Policy
     // withtout inlining assets as base64:
